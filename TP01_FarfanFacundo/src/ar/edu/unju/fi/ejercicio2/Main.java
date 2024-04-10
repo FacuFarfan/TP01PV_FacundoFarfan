11501c34ff30e13a3e3fd59f196767391b256bde
facul <facul@DESKTOP-38HS43L>
2024-04-10 16:11:32 -0300

    Ejercicio 2

---
+++ TP01_FarfanFacundo/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+	public static void main(String[] args) {
+		String country = "Argentina";
+		int age = 23;
+		double height = 50.5;
+		double price = 9.99;
+		String number = "3884770382";
+		double cos = Math.cos(0.5);
+		
+		System.out.println("Pais: " + country);
+		System.out.println("Edad: " + age + " años");
+		System.out.println("Altura del edificio: " + height + " metros");
+		System.out.println("Precio del producto: " + price + " dolares");
+		System.out.println("Numero de telefono: " + number);
+		System.out.println("Coseno de 0.5 es: " + cos);
+	}
+	
+}
