029ed057bae95dccc452d05c36d5ca5dfb0ec94d
facul <facul@DESKTOP-38HS43L>
2024-04-18 20:39:02 -0300

    Ejercicio 11

---
+++ TP01_FarfanFacundo/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,9 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+	public static void main(String[] args) {
+		for(int j=40, i=2;j>=0;j-=1,i+=1) {
+			System.out.print((j * i)+" ");
+		}
+	}
+}
